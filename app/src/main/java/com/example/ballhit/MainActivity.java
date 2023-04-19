bc5cb817fab8099e5724f8e37c17c645a95db853
micakoo <micakoo0112@gmail.com>
2023-04-19 16:03:43 +0800

    for login and game_over and play background change

--- app/src/main/java/com/example/ballhit/MainActivity.java
+++ app/src/main/java/com/example/ballhit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ballhit;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,11 +16,20 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
     //声明需要的组件
     private Button login,exit,reg;
     private  EditText   username,password;
     private SharedPreferences share;//声明SharedPreferences
+
+    private static final String FILENAME = "login_info";
+    private static final String USER_PREFS = "user_data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,23 +53,39 @@ public class MainActivity extends AppCompatActivity {
                 //判断输入信息是否为空
                 if(name.trim().equals("") || pass.trim().equals("")) {
                     Toast.makeText(MainActivity.this, "Username and password cannot be empty", Toast.LENGTH_LONG).show();
-                }
-                //获取保存文件中的用户名和密码
-                String savedUsername = share.getString("username","");
-                String savedPassword = share.getString("password","");
-                //查看输入的密码和名字是否一致
-                if(name.trim().equals(savedUsername) && pass.trim().equals(savedPassword)) {
-                    Toast.makeText(MainActivity.this, "WELCOME TO BALL HIT！", Toast.LENGTH_LONG).show();
-
-                    //成功登陆，进入LoginokActivity界面
-                    Intent intent=new Intent(MainActivity.this,LoginokActivity.class);
-                    startActivity(intent);
-                    finish();
-
                 } else {
-                    //错误的话
-                    Toast.makeText(MainActivity.this, "User name or password is wrong, please confirm the information or register", Toast.LENGTH_LONG).show();
-                    return;
+                    //检查用户名和密码是否匹配
+                    try {
+                        FileInputStream fis = openFileInput(FILENAME);
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+                        String line;
+                        boolean found = false;
+                        while ((line = reader.readLine()) != null) {
+                            String[] data = line.split(",");
+                            if (data.length == 2 && data[0].equals(name) && data[1].equals(pass)) {
+                                SharedPreferences preferences = getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = preferences.edit();
+                                editor.putString("username", name);
+                                editor.apply();
+                                Toast.makeText(MainActivity.this, "WELCOME TO BALL HIT！", Toast.LENGTH_LONG).show();
+                                //成功登陆，进入LoginokActivity界面
+                                Intent intent=new Intent(MainActivity.this,LoginokActivity.class);
+                                intent.putExtra("username", name); // Pass the user name to the next activity
+                                startActivity(intent);
+                                finish();
+                                found = true;
+                                break;
+                            }
+                        }
+                        fis.close();
+                        if (!found) {
+                            //错误的话
+                            Toast.makeText(MainActivity.this, "User name or password is wrong, please confirm the information or register", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -75,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
             finish();//结束一个Activity
         }
     }
+
     private class RegListenerimp implements View.OnClickListener {
 
         @Override
@@ -88,6 +115,23 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "WARNING：When registering, neither the username nor the password can be empty！", Toast.LENGTH_LONG).show();
                 return;//为空就会返回
             }
+            //检查是否已经注册过
+            try {
+                FileInputStream fis = openFileInput(FILENAME);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] data = line.split(",");
+                    if (data.length == 2 && data[0].equals(nam) && data[1].equals(pas)) {
+                        Toast.makeText(MainActivity.this, "This username has already been registered!", Toast.LENGTH_LONG).show();
+                        fis.close();
+                        return;
+                    }
+                }
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             //进入注册的Dialog
             Dialog dialog=new AlertDialog.Builder(MainActivity.this)
                     .setTitle("Register")
@@ -95,16 +139,10 @@ public class MainActivity extends AppCompatActivity {
                     .setPositiveButton("Confirm", new  DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            // TODO Auto-generated method stub
-                            //保存输入的信息 	 Editor 别忘了		edit.commit();提交
-                            share=getSharedPreferences("info",Activity.MODE_PRIVATE);
-                            SharedPreferences.Editor edit=share.edit();
-                            edit.putString("username", nam);
-                            edit.putString("password", pas);
-                            edit.commit();
+                            // 保存输入的信息到文件
+                            saveLoginInfoToFile(nam, pas);
                             //提示成功注册
                             Toast.makeText(MainActivity.this, "Congratulations, registration is successful!", Toast.LENGTH_LONG).show();
-
                         }
                     }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
 
@@ -116,7 +154,19 @@ public class MainActivity extends AppCompatActivity {
             dialog.show();//显示对话框，否者不成功
         }
 
+        private void saveLoginInfoToFile(String username, String password) {
+            try {
+                FileOutputStream fos = openFileOutput(FILENAME, MODE_APPEND);
+                String data = username + "," + password + "\n";
+                fos.write(data.getBytes());
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+
 
 
     //实现写一个admin 123456的用户
